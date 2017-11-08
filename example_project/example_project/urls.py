71147071dc2bcec36bda9d6455783c3b84b7957b
Alasdair Nicol <alasdair@thenicols.net>
2017-11-08 01:01:24 +0000

    Update admin include to work with Django 2.0
    We can simplify code after dropping support for Django 1.8

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,11 +1,17 @@
+import django
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+if django.VERSION[:2] == (1, 8):
+    admin_urls = include(admin.site.urls)
+else:
+    admin_urls = admin.site.urls
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin_urls),
     url(r'^photologue/', include('photologue.urls')),
     url(r'^$', TemplateView.as_view(template_name="homepage.html"), name='homepage'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
